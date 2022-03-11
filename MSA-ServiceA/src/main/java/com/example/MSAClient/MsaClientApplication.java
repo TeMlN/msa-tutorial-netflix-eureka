af1fd4c36ba59db34c7769f52e6dad4ad55b41ba
taem1n1 <youman7953@naver.com>
2022-03-11 15:08:26 +0900

    LoadBalanced 와 EnableDiscoveryClient 어노테이션 추가

--- MSA-ServiceA/src/main/java/com/example/MSAClient/MsaClientApplication.java
+++ MSA-ServiceA/src/main/java/com/example/MSAClient/MsaClientApplication.java
@@ -2,12 +2,23 @@ package com.example.MSAClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsaClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsaClientApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+
+		return new RestTemplate();
+	}
 }
---
+++ MSA-ServiceA/src/main/java/com/example/MSAClient/controller/HealthCheckController.java
@@ -0,0 +1,2 @@
+package com.example.MSAClient.controller;public class HealthCheckController {
+}
