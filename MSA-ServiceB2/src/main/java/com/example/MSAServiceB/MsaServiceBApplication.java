7593c6a3c0840f7ba5fa60c1509cdde92954d3df
taem1n1 <youman7953@naver.com>
2022-03-11 15:09:50 +0900

    initial commit

---
+++ MSA-ServiceB2/src/main/java/com/example/MSAServiceB/MsaServiceBApplication.java
@@ -0,0 +1,13 @@
+package com.example.MSAServiceB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaServiceBApplication.class, args);
+	}
+
+}
---
+++ MSA-ServiceB2/src/test/java/com/example/MSAServiceB/MsaServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSAServiceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
