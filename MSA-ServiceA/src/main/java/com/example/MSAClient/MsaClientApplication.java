6b600c29373e2e63618a4e6159cc39a3873c17c2
taem1n1 <youman7953@naver.com>
2022-03-11 14:41:35 +0900

    initial commit

---
+++ MSA-ServiceA/src/main/java/com/example/MSAClient/MsaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.MSAClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaClientApplication.class, args);
+	}
+
+}
