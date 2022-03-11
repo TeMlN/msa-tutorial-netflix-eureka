f96a490aa959c1cecbaf31e93fccea3724c5d857
taem1n1 <youman7953@naver.com>
2022-03-11 15:08:51 +0900

    Health Check Controller

--- MSA-ServiceA/src/main/java/com/example/MSAClient/controller/HealthCheckController.java
+++ MSA-ServiceA/src/main/java/com/example/MSAClient/controller/HealthCheckController.java
@@ -1,2 +1,15 @@
-package com.example.MSAClient.controller;public class HealthCheckController {
+package com.example.MSAClient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String checkState() {
+        return "Service-B: inst001 정상";
+    }
 }
