01f140c2372658e0ab1a198c36164231505122a6
taem1n1 <youman7953@naver.com>
2022-03-11 15:13:16 +0900

    rename

--- MSA-EurekaServer/src/main/java/com/example/MsaServer/MsaApplication.java
+++ MSA-EurekaServer/src/main/java/com/example/MsaServer/MsaApplication.java
@@ -1,4 +1,4 @@
-package com.example.msa;
+package com.example.MsaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- MSA-EurekaServer/src/test/java/com/example/MsaServer/MsaApplicationTests.java
+++ MSA-EurekaServer/src/test/java/com/example/MsaServer/MsaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.msa;
+package com.example.MsaServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
