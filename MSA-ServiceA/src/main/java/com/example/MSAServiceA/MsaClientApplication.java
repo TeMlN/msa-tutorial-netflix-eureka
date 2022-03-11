f47672f8f572a06c5a93d8581e1cd9e09ebd16ed
taem1n1 <youman7953@naver.com>
2022-03-11 15:19:16 +0900

    rename

--- MSA-ServiceA/src/main/java/com/example/MSAServiceA/MsaClientApplication.java
+++ MSA-ServiceA/src/main/java/com/example/MSAServiceA/MsaClientApplication.java
@@ -1,4 +1,4 @@
-package com.example.MSAServiceB1;
+package com.example.MSAServiceA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
