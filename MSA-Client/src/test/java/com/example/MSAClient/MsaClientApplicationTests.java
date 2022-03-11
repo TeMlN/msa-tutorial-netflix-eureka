1e40837b9efdac5fe808840bdbc6209934f7ac5f
taem1n1 <youman7953@naver.com>
2022-03-11 14:27:00 +0900

    initial commit

---
+++ MSA-Client/src/test/java/com/example/MSAClient/MsaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSAClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
