e638f1be1641166b7b1bf12f93323fffbb38089f
taem1n1 <youman7953@naver.com>
2022-03-14 11:09:18 +0900

    initial commit

---
+++ MSA-APIGateWay/src/main/java/com/example/MSAAPIGateWay/MsaApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.MSAAPIGateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaApiGateWayApplication.class, args);
+	}
+
+}
---
+++ MSA-APIGateWay/src/test/java/com/example/MSAAPIGateWay/MsaApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSAAPIGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
